15937c078cc7ce4701509a6c3af01ca927619957
Jan Christian Schwedhelm <50140958+janschwedhelm@users.noreply.github.com>
2021-12-08 12:48:57 +0100

    Update train-mnist-vae.sh

--- scripts/models/train-mnist-vae.sh
+++ scripts/models/train-mnist-vae.sh
@@ -4,7 +4,7 @@
 gpu="--gpu"  # change to "" if no GPU is to be used
 seed=0
 root_dir="logs/train"
-digit_array=( 0 1 2 3 4 5 6 7 8 9 )
+digit_array=( 3 )
 
 # Latent dimension = 2
 for d in "${digit_array[@]}"; do
